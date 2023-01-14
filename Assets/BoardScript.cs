5ab65410e904d274b25bec7e20139a921e0ceef2
erobert <etiennerobert33@gmail.com>
2023-01-14 20:18:26 +0100

    Spawn dice from prefab

--- Assets/BoardScript.cs
+++ Assets/BoardScript.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class BoardScript : MonoBehaviour
 {
+    public GameObject dicePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Hello World!");
+        Instantiate(dicePrefab, transform.position, transform.rotation);
     }
 
     // Update is called once per frame
